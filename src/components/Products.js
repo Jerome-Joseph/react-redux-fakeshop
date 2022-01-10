b05456f81ef3746bd795b4b9dab1dab48647a96d
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-01-10 17:56:45 +0530

    commit 3

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 const Products = () => {
-    const [product, setProduct] = useState([])
-
+    const [data, setData] = useState([])
+    const [filter, setFilter] = useState(data)
+   
     const fetchData = async() =>{
-        let data = await fetch('https://fakestoreapi.com/products')
-        let parsedData = await data.json()
-        setProduct(product.concat(parsedData))
-        console.log(parsedData);
+        let response = await fetch('https://fakestoreapi.com/products')
+        setData(await response.clone().json())
+        setFilter(await response.json());
+        // console.log(await response.json());
+    }
+    const filterData = async(category) =>{
+        let response = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+        setFilter(await response.json());
+        // console.log(await response.json());
     }
     useEffect(() => {
         fetchData();
@@ -15,9 +21,29 @@ const Products = () => {
     return (
         <div className='container text-center mt-5'>
             <h2>All Products</h2>
-            {product.map((element)=>{
-                <h1>{element.title}</h1>
+            <div className="buttons mt-4">
+            <button type="button" className="btn btn-dark mx-2 btn-lg" onClick={fetchData}>All</button>
+            <button type="button" className="btn btn-dark mx-2 btn-lg" onClick={()=>filterData("men's clothing")}>Men's Clothing</button>
+            <button type="button" className="btn btn-dark mx-2 btn-lg" onClick={()=>filterData("women's clothing")}>Women's Clothing</button>
+            <button type="button" className="btn btn-dark mx-2 btn-lg" onClick={()=>filterData("jewelery")}>Jewlery</button>
+            <button type="button" className="btn btn-dark mx-2 btn-lg" onClick={()=>filterData("electronics")}>Electronics</button>
+            </div>
+            <div className="row mt-5">
+            {filter.map((product)=>{
+                return(
+                        <div className="col-md-3 mb-4" key={product.id}>
+                        <div className="card h-100 text-center p-4">
+                            <img src={product.image} className="card-img-top" alt="..." style={{height:"250px"}}/>
+                            <div className="card-body">
+                                <h5 className="card-title mb-0">{product.title.substring(0,12)}</h5>
+                                <p className="card-text">Price ${product.price}</p>
+                                <a href="/" className="btn btn-primary">Add to Cart</a>
+                            </div>
+                            </div>
+                        </div> 
+                )
             })}
+             </div>
         </div>
     )
 }
